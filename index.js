02b3d08da590702433e954769b00af05baedd744
hridoy-ahmmad <hridoyp338y@gmail.com>
2025-11-10 11:01:42 +0600

    besic setup

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = 3000
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = "mongodb+srv://assignment10:Y5OweFJTUT3vIOjn@cluster0.yzzlbw3.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db('assignment10')
+    const carsCollection = db.collection('cars_collection')
+
+
+    app.get('/cars', (req, res)=>{
+      res.send('paichi vai')
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!!')
